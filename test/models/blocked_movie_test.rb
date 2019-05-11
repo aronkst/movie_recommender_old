25a463a487b5534ec1527db4821d64a69ab22817
Aron <aron.0504@hotmail.com>
2019-05-11 17:08:50 -0300

    test model blocked_movie

--- test/models/blocked_movie_test.rb
+++ test/models/blocked_movie_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class BlockedMovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save an empty locked movie' do
+    blocked_movie = BlockedMovie.new
+    assert_not blocked_movie.save
+  end
+
+  test 'should save a locked movie with an imdb' do
+    blocked_movie = BlockedMovie.new(imdb: 'tt4154796')
+    assert blocked_movie.save
+  end
+
+  test 'should not save a locked movie with the same imdb' do
+    BlockedMovie.create(imdb: 'tt4154796')
+    blocked_movie = BlockedMovie.new(imdb: 'tt4154796')
+    assert_not blocked_movie.save
+  end
 end
