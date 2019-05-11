f08bec95ff4eaf9178e386daa794847567cd1927
Aron <aron.0504@hotmail.com>
2019-05-11 17:32:17 -0300

    test model recommended_movie in english

--- test/models/recommended_movie_test.rb
+++ test/models/recommended_movie_test.rb
@@ -1,32 +1,32 @@
 require 'test_helper'
 
 class RecommendedMovieTest < ActiveSupport::TestCase
-  test 'não deve salvar um filme recomendado vazio' do
+  test 'should not save an empty recommended movie' do
     recommended_movie = RecommendedMovie.new()
     assert_not recommended_movie.save
   end
 
-  test 'não deve salvar um filme recomendado apenas com a coluna imdb' do
+  test 'should not save a movie recommended only with the imdb column' do
     recommended_movie = RecommendedMovie.new(imdb: 'tt4154796')
     assert_not recommended_movie.save
   end
 
-  test 'não deve salvar um filme recomendado apenas com a coluna recommended_imdb' do
+  test 'should not save a movie recommended only with the recommended_imdb column' do
     recommended_movie = RecommendedMovie.new(recommended_imdb: 'tt4154796')
     assert_not recommended_movie.save
   end
 
-  test 'não deve salvar um filme recomendado com a coluna imdb e recommended_imdb com valores iguais' do
+  test 'should not save a recommended movie with the imdb and recommended_imdb column with equal values' do
     recommended_movie = RecommendedMovie.new(imdb: 'tt4154796', recommended_imdb: 'tt4154796')
     assert_not recommended_movie.save
   end
 
-  test 'deve salvar o filme recomendado com todos os valores preenchidos' do
+  test 'should save the recommended movie with all the values filled in' do
     recommended_movie = RecommendedMovie.new(imdb: 'tt4154796', recommended_imdb: 'tt4154756')
     assert recommended_movie.save
   end
 
-  test 'não deve salvar um filme recomendado se ja existir uma linha com a coluna imdb e recommended_imdb com os mesmos valores' do
+  test 'should not save a recommended movie if there is already a line with the imdb and recommended_imdb column with the same values' do
     RecommendedMovie.create(imdb: 'tt4154796', recommended_imdb: 'tt4154756')
     recommended_movie = RecommendedMovie.new(imdb: 'tt4154796', recommended_imdb: 'tt4154756')
     assert_not recommended_movie.save
