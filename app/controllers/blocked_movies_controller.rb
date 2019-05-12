5f65a1aad87edac3d5f000abc3e16ab95332c9e8
Aron <aron.0504@hotmail.com>
2019-05-12 10:29:25 -0300

    fix routes and blocked_movies controller

--- app/controllers/blocked_movies_controller.rb
+++ app/controllers/blocked_movies_controller.rb
@@ -1,4 +1,21 @@
 class BlockedMoviesController < ApplicationController
+  before_action :set_title
+
   def index
+    @blocked_movies = BlockedMovie.includes(:movie).all
+  end
+
+  def delete
+    blocked_movie = BlockedMovie.find_by_imdb(params[:imdb])
+    unless blocked_movie.delete
+      flash[:error] = 'Unable to remove this movie from your blocked movies'
+    end
+    redirect_to blocked_movies_index_path
+  end
+
+  private
+
+  def set_title
+    @title = 'Blocked Movies'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get 'most_popular_movies/:imdb/new', to: 'most_popular_movies#new', as: 'most_popular_movies_new'
   post 'most_popular_movies/:imdb/new', to: 'most_popular_movies#create', as: 'most_popular_movies_create'
 
-  get 'removed_movies', to: 'removed_movies#index', as: 'removed_movies_index'
-  delete 'removed_movies/:imdb/delete', to: 'removed_movies#delete', as: 'removed_movies_delete'
+  get 'blocked_movies', to: 'blocked_movies#index', as: 'blocked_movies_index'
+  delete 'blocked_movies/:imdb/delete', to: 'blocked_movies#delete', as: 'blocked_movies_delete'
 
   get 'watched_movies', to: 'watched_movies#index', as: 'watched_movies_index'
   get 'watched_movies/:imdb/edit', to: 'watched_movies#edit', as: 'watched_movies_edit'
