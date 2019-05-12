81f8175b2aeb7ced42610a6a216bb08b8b92e669
Aron <aron.0504@hotmail.com>
2019-05-12 10:22:28 -0300

    routes and most_popular_movies controller

--- app/controllers/most_popular_movies_controller.rb
+++ app/controllers/most_popular_movies_controller.rb
@@ -1,7 +1,35 @@
 class MostPopularMoviesController < ApplicationController
+  before_action :set_title_index, only: :index
+  before_action :set_title_new_create, except: :index
+
   def index
+    @most_popular_movies = CrawlerMostPopularMovies.new.load
   end
 
   def new
+    @watched_movie = WatchedMovie.new(imdb: params[:imdb])
+  end
+
+  def create
+    @watched_movie = WatchedMovie.new(wathed_movie_params)
+    if @wathed_movie.save
+      redirect_to watched_movies_index_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_title_index
+    @title = "Most Popular Movies"
+  end
+
+  def set_title_new_create
+    @title = "Add #{params[:title]}"
+  end
+
+  def wathed_movie_params
+    params.require(:watched_movie).permit(:imdb, :score)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  get 'watched_movies/index'
-  get 'watched_movies/edit'
-  get 'search/index'
-  get 'search/new'
-  get 'blocked_movies/index'
-  get 'recommended_movies/index'
-  get 'recommended_movies/new'
-  get 'most_popular_movies/index'
-  get 'most_popular_movies/new'
+  get 'search', to: 'search#index', as: 'search_index'
+  get 'search/:imdb/new', to: 'search#new', as: 'search_new'
+  post 'search/:imdb/new', to: 'search#create', as: 'search_create'
+
+  get 'most_popular_movies', to: 'most_popular_movies#index', as: 'most_popular_movies_index'
+  get 'most_popular_movies/:imdb/new', to: 'most_popular_movies#new', as: 'most_popular_movies_new'
+  post 'most_popular_movies/:imdb/new', to: 'most_popular_movies#create', as: 'most_popular_movies_create'
+
+  get 'removed_movies', to: 'removed_movies#index', as: 'removed_movies_index'
+  delete 'removed_movies/:imdb/delete', to: 'removed_movies#delete', as: 'removed_movies_delete'
+
+  get 'watched_movies', to: 'watched_movies#index', as: 'watched_movies_index'
+  get 'watched_movies/:imdb/edit', to: 'watched_movies#edit', as: 'watched_movies_edit'
+  patch 'watched_movies/:imdb/edit', to: 'watched_movies#update', as: 'watched_movies_update'
+  delete 'watched_movies/:imdb/delete', to: 'watched_movies#delete', as: 'watched_movies_delete'
+
+  get 'recommended_movies', to: 'recommended_movies#index', as: 'recommended_movies_index'
+  get 'recommended_movies/:imdb/new', to: 'recommended_movies#new', as: 'recommended_movies_new'
+  post 'recommended_movies/:imdb/new', to: 'recommended_movies#create', as: 'recommended_movies_create'
+
+  root 'welcome#index'
+
+  mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
