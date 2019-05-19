0522ce1b38e5e3a6dc5c276633ddb79d1f2f220b
Aron <aron.0504@hotmail.com>
2019-05-19 15:04:52 -0300

    show_post helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
       ''
     end
   end
+
+  def show_poster(model, css_class = 'img-thumbnail', variant = nil)
+    return if model.nil?
+
+    return if model.image.nil?
+
+    return unless model.image.attached?
+
+    if variant.nil?
+      image_tag(model.image, class: css_class)
+    else
+      image_tag(model.image.variant(resize: variant), class: css_class)
+    end
+  end
 end
