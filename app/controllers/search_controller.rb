f48e8750a2385f338f178915bb65e6b0e70434d9
Aron <aron.0504@hotmail.com>
2019-05-27 22:37:02 -0300

    organized html

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,14 @@
 class SearchController < ApplicationController
-  before_action :check_params_search, only: :index
-  before_action :title_index, only: :index
+  before_action :title_index
 
   def index
+    @movies = []
+    return unless params[:search]
+
     @movies = CrawlerSearchMovie.new.load(params[:search])
   end
 
   private
-
-  def check_params_search
-    return unless params[:search].nil? || params[:search].strip.empty?
-
-    flash[:error] = 'Could not search because the search word was not found'
-    redirect_to root_path
-  end
 
   def title_index
     @title = "Search By #{params[:search]}"
