b3b2f71b752be0fe00f14614edb3e37dc2667d2b
Aron <aron.0504@hotmail.com>
2019-06-04 23:07:02 -0300

    most popular movies tests

--- test/controllers/most_popular_movies_controller_test.rb
+++ test/controllers/most_popular_movies_controller_test.rb
@@ -4,7 +4,10 @@ class MostPopularMoviesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get most_popular_movies_index_url
     assert_response :success
-    # Title?
-    # Movies?
+
+    assert_select 'title', 'Most Popular Movies - Movie Recommender'
+
+    crawler_movies = CrawlerMostPopularMovies.new.load
+    assert_select 'h5', crawler_movies.length
   end
 end
