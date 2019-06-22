1d9b7947bb07d47bc2e225a05fe88e06a36a0c67
Aron <aron.0504@hotmail.com>
2019-06-22 13:37:36 -0300

    fix score from movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   validates :imdb, :score, :title, :summary, :year, :genres, presence: true
   validates :imdb, uniqueness: true
-  validates :score, inclusion: 1..10
+  validates :score, inclusion: 0..10
   validates :year, numericality: { greater_than: 0 }
   has_one_attached :image
 end
