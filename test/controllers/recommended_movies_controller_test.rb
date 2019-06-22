a8b4981731853bc95ea25ab23c79eb6d47b5364e
Aron <aron.0504@hotmail.com>
2019-06-22 16:21:23 -0300

    recommended movies controller

--- test/controllers/recommended_movies_controller_test.rb
+++ test/controllers/recommended_movies_controller_test.rb
@@ -3,20 +3,9 @@ require 'test_helper'
 class RecommendedMoviesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get recommended_movies_index_url
+
     assert_response :success
-    # Title?
-    # Movies?
-  end
-
-  test 'should get block' do
-    pass
-    # Block?
-    # Redirect?
-  end
-
-  test 'should get block error' do
-    pass
-    # Error?
-    # Redirect?
+    assert_select 'title', 'Recommended Movies - Movie Recommender'
+    assert_select 'h5', 1
   end
 end
